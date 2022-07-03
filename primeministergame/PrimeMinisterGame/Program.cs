92a441e9d7a523cb0d4079d90511195484a8d108
Victoria Kotwicki <victoria.kotwicki@probablyrational.com>
2022-07-03 21:54:37 +0100

    initial commit

---
+++ primeministergame/PrimeMinisterGame/Program.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+namespace PrimeMinisterGame
+{
+	class Program
+	{
+		static void Main()
+		{
+			bool replay;
+
+			while (true)
+			{
+				// Set to true so loop will always run unless changed
+				replay = true;
+				Console.WriteLine("\nWhich game would like like to play?" +
+					"\n1. Which prime minister served first?" +
+					"\n2. Which prime minister served on this date?" +
+					"\n3. Exit.");
+				Console.Write(">> ");
+				// Makes sure user input is integer and between 1 an 3
+				if (int.TryParse(Console.ReadLine(), out int answer) && Enumerable.Range(1, 3).Contains(answer))
+				{
+					if (answer == 1)
+					{
+						// Creates new instance of class game
+						Game newGuessPMGame = new Game();
+						while (replay)
+						{
+							// Runs pm guessing game
+							replay = newGuessPMGame.InitFirstPMGame();
+						}
+					}
+					else if (answer == 2)
+					{
+						// Creates new instance of class game
+						Game newPMDateGame = new Game();
+						while (replay)
+						{
+							// Runs the date guessing game
+							replay = newPMDateGame.InitPMDateGame();
+						}
+					}
+					else { break; } // Answers can only be between 1 and 3 so 3rd option will always be exit
+				}
+				else { Console.WriteLine("Incorrect input, please enter 1, 2 or 3.\n"); }
+			}
+
+			Console.WriteLine("Thanks for playing.");
+			Console.ReadKey();
+		}
+	}
+
+	// Actual games
+	class Game
+	{
+		// Initiates pm guessing game, loading primister info into a list
+		public bool InitFirstPMGame()
+		{
+			string checkReplay;
+			int runs = 0;
+			bool flag;
+			List<List<string>> listOfPMs = GetData();
+			List<List<string>> chosenPMs = new List<List<string>>();
+
+			Random rnd = new Random();
+
+			Player score = new Player();
+			score.TotalScore = 0;
+
+			while (runs != 5)
+			{
+
+				// Get 3 random MPs from list to use
+				while (true)
+				{
+					// Cleans up list for ne
+					chosenPMs.Clear();
+					flag = true; // Resets flag
+
+					chosenPMs.Add(listOfPMs[rnd.Next(listOfPMs.Count)]);
+					chosenPMs.Add(listOfPMs[rnd.Next(listOfPMs.Count)]);
+					chosenPMs.Add(listOfPMs[rnd.Next(listOfPMs.Count)]);
+
+					try
+					{
+						CheckPMDups(chosenPMs);
+					}
+					catch (SameStringValueException)
+					{
+						flag = false;
+					}
+					// C# needs try-catch-else
+					if (flag == true) { break; }
+				}
+
+				Console.WriteLine("\nWhich one of these primisters served first?\n" +
+					"Please enter 1, 2 or 3.");
+				for (int i = 0; i < chosenPMs.Count; i++)
+				{
+					Console.WriteLine("{0}. {1}", i + 1, chosenPMs[i][1]);
+				}
+
+				while (true)
+				{
+					Console.Write(">> ");
+					// Checks if answer is in correct format (int)
+					if (int.TryParse(Console.ReadLine(), out int answer) && Enumerable.Range(1, 3).Contains(answer)) // Use range instead? TODO
+					{
+						// Don't know answer so checks against all 3 possible ones
+						if (int.Parse(chosenPMs[answer - 1][0]) <= int.Parse(chosenPMs[0][0]) && int.Parse(chosenPMs[answer - 1][0]) <= int.Parse(chosenPMs[1][0]) && int.Parse(chosenPMs[answer - 1][0]) <= int.Parse(chosenPMs[2][0]))
+						{
+							score.TotalScore += 1;
+							Console.WriteLine("Correct! Your current score is {0} out of 5", score.TotalScore);
+						}
+						else { Console.WriteLine("Incorrect! Your current score is {0} out of 5", score.TotalScore); }
+						break;
+					}
+					else
+					{
+						Console.WriteLine("Please only enter one number, 1, 2 or 3.\n");
+					}
+				}
+
+				// Emptys for next use
+				chosenPMs.Clear();
+				runs++;
+			}
+
+			Console.WriteLine("\nYou scored {0} out of 5\n", score.TotalScore);
+
+			Console.WriteLine("Would you like to play again? Y/N");
+			while (true)
+			{
+				Console.Write(">> ");
+				checkReplay = Console.ReadLine();
+				if (checkReplay == "Y" || checkReplay == "y") { return true; }
+				else if (checkReplay == "N" || checkReplay == "n") { return false; }
+				else { Console.WriteLine("Please enter either 'Y' or 'N' without the '"); }
+			}
+		}
+
+		// Initiates pm date guessing game, loading primister info into a list
+		public bool InitPMDateGame()
+		{
+			string checkReplay;
+			string date;
+			int pmAnswer;
+			int runs = 0;
+			bool flag;
+			List<List<string>> listOfPMs = GetData();
+			List<List<string>> chosenPMs = new List<List<string>>();
+
+			Random rnd = new Random();
+
+			Player score = new Player();
+			score.TotalScore = 0;
+
+			while (runs != 5)
+			{
+
+				// Get 3 random MPs from list to use
+				while (true)
+				{
+					// Cleans up lists for next use
+					chosenPMs.Clear();
+
+					flag = true; // Resets flag
+
+					chosenPMs.Add(listOfPMs[rnd.Next(listOfPMs.Count)]);
+					chosenPMs.Add(listOfPMs[rnd.Next(listOfPMs.Count)]);
+					chosenPMs.Add(listOfPMs[rnd.Next(listOfPMs.Count)]);
+
+					try
+					{
+						CheckPMDups(chosenPMs);
+					}
+					catch (SameStringValueException)
+					{
+						flag = false;
+					}
+					// C# needs try-catch-else
+					if (flag == true)
+					{
+						// Chooses random pm from list
+						pmAnswer = rnd.Next(0, 3);
+						// Gets a date to use for game
+						date = chosenPMs[pmAnswer][3];
+						break;
+					}
+				}
+
+				Console.WriteLine("\nWhich prime minister serving on {0}? " +
+					"Please enter 1, 2 or 3.", date);
+				for (int i = 0; i < chosenPMs.Count; i++)
+				{
+					Console.WriteLine("{0}. {1}", i + 1, chosenPMs[i][1]);
+				}
+
+				while (true)
+				{
+					Console.Write(">> ");
+					// Checks if answer is in correct format (int)
+					if (int.TryParse(Console.ReadLine(), out int answer) && Enumerable.Range(1,3).Contains(answer)) // Use range instead? TODO
+					{
+						// Checks if answer is correct
+						Console.WriteLine("{0}", chosenPMs[answer-1][3]);
+
+
+						if (chosenPMs[answer-1][3].ToString() == date)
+						{
+							score.TotalScore += 1;
+							Console.WriteLine("Correct! Your current score is {0} out of 5", score.TotalScore);
+						}
+						else { Console.WriteLine("Incorrect! Your current score is {0} out of 5", score.TotalScore); }
+						break;
+					}
+					else
+					{
+						Console.WriteLine("Please only enter one number, 1, 2 or 3.\n");
+					}
+				}
+
+				// Emptys for next use
+				chosenPMs.Clear();
+				runs++;
+			}
+
+			//Displays final score and asks if they want to play again
+			Console.WriteLine("\nYou scored {0} out of 5\n", score.TotalScore);
+			Console.WriteLine("Would you like to play again? Y/N");
+			while (true)
+			{
+				Console.Write(">> ");
+				checkReplay = Console.ReadLine();
+				if (checkReplay == "Y" || checkReplay == "y") { return true; }
+				else if (checkReplay == "N" || checkReplay == "n") { return false; }
+				else { Console.WriteLine("Please enter either 'Y' or 'N' without the '"); }
+			}
+		}
+		
+
+		// Gets priminster data from csv file
+		List<List<string>> GetData()
+		{
+			// List that stores lists of prime ministers and data
+			List<List<string>> listOfPMs = new List<List<string>>();
+
+			// Placeholder list
+			List<string> srcLine = new List<string>();
+
+			// Gets program's current path and looks for a file called "".csv to import
+			string path = @"list_of_prime_ministers_of_uk-1-UPDATE.csv";
+			bool flag = false;
+
+			while (true)
+			{
+				try
+				{
+					// Uses StreamReader to get each line from csv, seperating each line into a list by the ',', and adding that list into one big list of lists
+					using (StreamReader src = File.OpenText(path))
+					{
+						// Loops till nothing left to read
+						while (!src.EndOfStream)
+						{
+							listOfPMs.Add(src.ReadLine().Split(',').ToList());
+						}
+					}
+					flag = true;
+				}
+				catch (Exception)
+				{
+					// Allows user to add file o folder if not found
+					Console.WriteLine("File could not be read. Please make sure the 'list_of_prime_ministers_of_uk-1-UPDATE.csv' is in the same folder as the game.");
+					Console.WriteLine("Current dictionary: {0}", Directory.GetCurrentDirectory());
+					Console.WriteLine("Once you've copied it there, press any key to try again.\n");
+					Console.ReadKey();
+				}
+				if (flag == true) { break; }
+			}
+			//Removes headers from list
+			listOfPMs.RemoveAt(0);
+			return listOfPMs;
+		}
+
+		// Checks if any given strings have same value
+		static void CheckPMDups(List<List<string>> PMsList)
+		{
+			// COunt unnedded as lists will always have same lengths, order doesn't matter
+			if (PMsList[0][1].All(PMsList[1][1].Contains) || PMsList[0][1].All(PMsList[2][1].Contains) || PMsList[1][1].All(PMsList[2][1].Contains))
+			{
+				throw new SameStringValueException();
+			}
+		}
+	}
+
+	// Player score class
+	public class Player
+	{
+		// Player's score
+		public int TotalScore { get; set; }
+	}
+
+	// Exception class for showing error
+	public class SameStringValueException : Exception
+	{
+		public SameStringValueException() : base
+			("2 or more strings share the same value.") { }
+
+	}
+}
---
+++ rbg_converter/RGB_Converter/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+class Program
+{
+	static void Main()
+	{
+		// Variables list
+		string[] hexValue;
+		string[] letters = { "a", "b", "c", "d", "e", "f" };
+		bool flag;
+		bool cont = true;
+
+		// Loops till user exits program
+		while (cont)
+		{
+			// Resets variables on rerun
+			string[] hexInverted = { "", "", "" };
+			int[] rgbValue = { 0, 0, 0 };
+			int[] rgbInverted = { 0, 0, 0 };
+			int counter = 0;
+
+			// Loops till gets a valid response
+			while (true)
+			{
+				flag = true;
+				Console.Write("Please enter a colour's hexadecimal value: ");
+
+				hexValue = Regex.Replace(Console.ReadLine(), "[^A-Za-z0-9]", "").ToLower().ToCharArray() // Makes char array to get each character
+					.Select(c => c.ToString()).ToArray();// Converts the char array to string array
+
+				// Makes sure input is only 6 chars (symbols arent counted)
+				if (!(hexValue.Length == 6))
+				{
+					Console.WriteLine("Please make sure the hex value is 6 characters long (symbols aren't counted).");
+					continue;
+				}
+
+				// Error checking
+				for (int i = 0; i < 6; i++)
+				{
+					// if not an int and its a letter in the array
+					if (int.TryParse(hexValue[i].ToString(), out int value) == false)
+					{
+						// Checks to see if only letters A-F are used
+						if (!letters.Any(hexValue[i].ToString().Contains))
+						{
+							Console.WriteLine("Letters can only be between A-F");
+							flag = false;
+							break;
+						}
+					}
+				}
+				if (flag == true) { break; }
+			}
+
+			// Converts hex to rgb and inverts both
+			for (int i = 0; i < 3; i += 1)
+			{
+				// f=15^0 e=14^1 d=13^2 c=12^3 b=11^4 a=10^5 9^6
+				rgbValue[i] = Convert.ToInt32(hexValue[counter] + hexValue[counter + 1], 16);
+				rgbInverted[i] = 255 - rgbValue[i]; // Invertes rgb value
+				hexInverted[i] = rgbInverted[i].ToString("X").ToLower();
+				if (hexValue[counter] == "f") { hexInverted[i] = "0" + hexInverted[i]; }
+				counter += 2;
+			}
+			// Outputs results
+			Console.WriteLine("\nHex to RGB:");
+			Console.WriteLine("#{0}{1}{2}{3}{4}{5} : ({6}, {7}, {8})", hexValue[0], hexValue[1], hexValue[2], hexValue[3], hexValue[4], hexValue[5],
+				rgbValue[0], rgbValue[1], rgbValue[2]);
+
+			Console.WriteLine("\nInverted Value:");
+			Console.WriteLine("#{0}{1}{2} : ({3}, {4}, {5})", hexInverted[0], hexInverted[1], hexInverted[2],
+				rgbInverted[0], rgbInverted[1], rgbInverted[2]);
+
+			Console.WriteLine("\nMonochrome RGB value:");
+			// Converts rgb value to black or white and prints to console
+			if ((rgbValue[0] * 0.2126) + (rgbValue[1] * 0.7152) + (rgbValue[2] * 0.0722) < 128) { Console.WriteLine("(0, 0, 0)"); }
+			else { Console.WriteLine("(255, 255, 255)\n"); }
+
+			Console.WriteLine("Press c to continue or e to exit");
+
+			// Asks if user wants to continue using program
+			while (true)
+			{
+				var key = Console.ReadKey();
+				Console.WriteLine(""); // Tidy
+				// Causes program to repeat
+				if (key.KeyChar.ToString() == "c")
+				{
+					Console.Clear();
+					break;
+				}
+				// Causes program to terminate
+				else if (key.KeyChar.ToString() == "e")
+				{
+					cont = false;
+					break;
+				}
+				// Repeats this loop with invalid input
+				else { Console.WriteLine("Invalid key press, please try again"); }
+			}
+		}
+	}
+}
